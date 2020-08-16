a4fca02c866745c36d835700963ec0e46c2d957e
Nage Mandal <nagendra.mandal@gmail.com>
2020-08-16 02:07:12 -0700

    Status added

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("Status: Thank you ok!");
 });
 
 // retrieve all musicians from data store
